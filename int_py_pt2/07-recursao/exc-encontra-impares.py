6700f53a519b9f5079f1c45c371279153d79a56b
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-08-09 22:18:47 -0300

    encontra-impares recursivo antes de simplificação; parece estar funcionando bem.

---
+++ int_py_pt2/07-recursao/exc-encontra-impares.py
@@ -0,0 +1,24 @@
+import pytest
+
+def encontra_impares(lista):
+    return agrupar_impares(lista[:])
+
+def agrupar_impares(lista, apenas_impares=False):
+    if apenas_impares == True:
+        return lista
+    else:
+        impares = []
+        for i in lista:
+            if i % 2 != 0:
+                impares.append(i)
+        return agrupar_impares(impares, True)
+
+
+@pytest.mark.parametrize("lista, impares",[
+    ([1, 2, 3, 4, 5, 6, 7], [1, 3, 5, 7]),
+    ([1, 3, 5, 7], [1, 3, 5, 7]),
+    ([3, 2, 21, 44, 47, 89, 91, 8, 100], [3, 21, 47, 89, 91]),
+    ([21, 22, 23, 25, 28, 28, 29, 30], [21, 23, 25, 29])
+])
+def test_encontra_impares(lista, impares):
+    assert encontra_impares(lista) == impares
