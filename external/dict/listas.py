692119cb8a601c505da55818e343234758697f6e
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-09-27 10:43:40 -0300

    Implantando class Listas, para simplificar @pytest.fixture em "listas-test.py".

--- external/dict/listas.py
+++ external/dict/listas.py
@@ -1,33 +1,34 @@
-def criar_chaves(lista):
-	dicionario = {}
-	for i in lista:
-		dicionario[i] = ""
-	return dicionario
+class Listas:
+    def criar_chaves(lista):
+        dicionario = {}
+        for i in lista:
+            dicionario[i] = ""
+        return dicionario
 
-def atribuir_valores(dicionario, lista, novo=False):
-    assert len(dicionario) == len(lista)
-    dicionario_final = dicionario
-    if novo == True:
-        dicionario_final = {}
-    c = 0
-    for i in dicionario:
-        dicionario_final[i] = lista[c]
-        c += 1
-    return dicionario_final
+    def atribuir_valores(dicionario, lista, novo=False):
+        assert len(dicionario) == len(lista)
+        dicionario_final = dicionario
+        if novo == True:
+            dicionario_final = {}
+        c = 0
+        for i in dicionario:
+            dicionario_final[i] = lista[c]
+            c += 1
+        return dicionario_final
 
-def criar_dicionario(lista_chaves, lista_valores):
-    dicionario = criar_chaves(lista_chaves)
-    atribuir_valores(dicionario, lista_valores)
-    return dicionario
+    def criar_dicionario(lista_chaves, lista_valores):
+        dicionario = Listas.criar_chaves(lista_chaves)
+        Listas.atribuir_valores(dicionario, lista_valores)
+        return dicionario
 
-def obter_chaves(dicionario):
-    lista_chaves = []
-    for i in dicionario:
-        lista_chaves.append(i)
-    return lista_chaves
+    def obter_chaves(dicionario):
+        lista_chaves = []
+        for i in dicionario:
+            lista_chaves.append(i)
+        return lista_chaves
 
-def obter_valores(dicionario):
-    lista_valores = []
-    for i in dicionario:
-        lista_valores.append(dicionario[i])
-    return lista_valores
+    def obter_valores(dicionario):
+        lista_valores = []
+        for i in dicionario:
+            lista_valores.append(dicionario[i])
+        return lista_valores
