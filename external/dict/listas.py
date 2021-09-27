3f982673bdcd6684386eb4ecc218d621993ba8e5
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-09-27 07:57:32 -0300

    Modificada a função atribuir_valores: agora possui um terceiro parâmetro, "novo", que recebe um valor booleano: se for passado True, a função não modificará o dicionário passado como argumento, mas criará um novo dicionário, deixando o dicionário original intacto; caso contrário, a função modificará o dicionário passado como argumento, como fazia anteriormente (comportamento padrão).

--- external/dict/listas.py
+++ external/dict/listas.py
@@ -4,13 +4,17 @@ def criar_chaves(lista):
 		dicionario[i] = ""
 	return dicionario
 
-def atribuir_valores(dicionario, lista):
+def atribuir_valores(dicionario, lista, novo=False):
     assert len(dicionario) == len(lista)
+    if novo == True:
+        dicionario_final = {}
+    elif novo == False:
+        dicionario_final = dicionario
     c = 0
     for i in dicionario:
-        dicionario[i] = lista[c]
+        dicionario_final[i] = lista[c]
         c += 1
-    return dicionario
+    return dicionario_final
 
 def criar_dicionario(lista_chaves, lista_valores):
     dicionario = criar_chaves(lista_chaves)
