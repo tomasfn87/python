636d2cb1e46a542fb1680a8b5167f2da41b9f0c1
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-11-18 17:49:57 -0300

    1): implantada a chamada da função "Listas.sortArrCaseIns()" em "Listas.sortDictArrByKey()". 2): alterados os comentários das funções: 2.1): "Listas.sortArrCaseIns()" e 2.2): "Listas.sortArrListCaseIns()": a função "Listas.sortDictArrByKey()" usa 2.1) (e não 2.2 como relatava o comentando de 2.1) anteriormente).

--- external/dict/listas.py
+++ external/dict/listas.py
@@ -36,63 +36,53 @@ class Listas:
     def sortArrCaseIns(arr, inv=0, noCase=0, targetArr=[]):
         '''This function uses Bubble Sort to sort a list ("arr"):
 
-        * if inv receives True, the sorting will be reversed;
+        * if "inv" receives "True", the sorting will be reversed;
 
-        * if noCase receives True, the sorting will group letters 
-            ignoring upper and lower cases in a first moment, and
-            then take it into account again to group the same letters:
-            first upper case letters and the lower case letters.
-            Example: "AbCa" returns "AabC"
-            (list.sort() would return "ACab")
-        
+        * if "noCase" receives "True", the sorting will group letters
+        ignoring upper and lower cases in a first moment, and then will
+        take it into account again to group the same letters: first 
+        upper case letters and the lower case letters.
+        Example: "['A','b','C','a']" returns "['A','a','b','C']"
+        (list.sort() would return "['A','C','a','b']");
+
         * if "targetArr" receives a valid argument, the function will
         check if it's a list and if its length is the same as "arr": 
         if the result is True, the function will sort the second list
         based on the first list, returning a copy of the second list,
         that may even end up unordered, depending on the correlation 
-        between "arr" and "target". This functionality was designed to
-        be used directly by "Listas.sortArrListCaseIns()", which does
-        basically the same thing, but instead of receiving two arrays 
-        ("arr", "targetArr"), it receives two list of arrays
-        ("arrList", "targetArrList").
-
-        * "Listas.sortArrListCaseIns()" itself was designed to take
-        advantage of "Listas.sortArrCaseIns()"'s "targetArr" parameter
-        and remove the Bubble Sort algorithm from "Listas.sortDictsByKey()"
+        between "arr" and "targetArr" - the problem is solved if the
+        lists are copies of each other slightly modified, for example.
         '''
         if targetArr:
             if type(targetArr) != list or len(arr) != len(targetArr):
                 return False
-            sortedArr = targetArr[:]
+            sortArr = targetArr[:]
         else:
-            sortedArr = arr[:]
+            sortArr = arr[:]
         refArr = arr[:]
-        for i in range(1, len(arr)):
-            for j in range(0, len(arr) - i):
+        for i in range(1, len(refArr)):
+            for j in range(0, len(refArr) - i):
                 if noCase:
                     if refArr[j].lower() == refArr[j+1].lower()\
                         and refArr[j] > refArr[j+1]:
-                        refArr[j], refArr[j+1] =\
-                            refArr[j+1], refArr[j]
-                        sortedArr[j], sortedArr[j+1] =\
-                            sortedArr[j+1],sortedArr[j]
+                        refArr[j], refArr[j+1] = refArr[j+1], refArr[j]
+                        sortArr[j], sortArr[j+1] = sortArr[j+1],sortArr[j]
                     elif refArr[j].lower() > refArr[j+1].lower():
-                        refArr[j], refArr[j+1] =\
-                            refArr[j+1], refArr[j]
-                        sortedArr[j], sortedArr[j+1] =\
-                            sortedArr[j+1], sortedArr[j]
+                        refArr[j], refArr[j+1] = refArr[j+1], refArr[j]
+                        sortArr[j], sortArr[j+1] = sortArr[j+1], sortArr[j]
                 else:
                     if refArr[j] > refArr[j+1]:
-                        refArr[j], refArr[j+1] =\
-                            refArr[j+1], refArr[j]
-                        sortedArr[j], sortedArr[j+1] =\
-                            sortedArr[j+1], sortedArr[j]
+                        refArr[j], refArr[j+1] = refArr[j+1], refArr[j]
+                        sortArr[j], sortArr[j+1] = sortArr[j+1], sortArr[j]
             i += 1
         if inv:
-            sortedArr.reverse()
-        return sortedArr
+            sortArr.reverse()
+        return sortArr
 
     def sortArrListCaseIns(arrListEntry, inv=0, noCase=0, targetArrList=0):
+        '''Similar to "Listas.sortArrCaseIns()", but instead of sorting
+        a single array, it will sort an array of arrays.
+        * CaseIns = Case Insensitive'''
         if not Listas.checarListaListas(arrListEntry):
             return False
         arrList = []
@@ -110,38 +100,20 @@ class Listas:
 
     def sortDictArrByKey(dictArr, key, inv=0, noCase=0):
         # receives an array containing dictionaries that contain the
-        # informed key
+        # informed key and sort the dictionary list based on each
+        # item's value for the specified key
         if not Listas.dictsHaveKey(dictArr, key):
             return False
         sortedDictArr = dictArr[:]
         values = []
         for d in dictArr:
             values.append(d[key])
-        for i in range(1, len(values)):
-            for j in range(0, len(values) - i):
-                if noCase:
-                    if values[j].lower() == values[j+1].lower()\
-                        and values[j] > values[j+1]:
-                        values[j], values[j+1] = values[j+1], values[j]
-                        sortedDictArr[j], sortedDictArr[j+1] =\
-                            sortedDictArr[j+1], sortedDictArr[j]
-                    elif values[j].lower() > values[j+1].lower():
-                        values[j], values[j+1] = values[j+1], values[j]
-                        sortedDictArr[j], sortedDictArr[j+1] =\
-                            sortedDictArr[j+1], sortedDictArr[j]
-                else:
-                    if values[j] > values[j+1]:
-                        values[j], values[j+1] = values[j+1], values[j]
-                        sortedDictArr[j], sortedDictArr[j+1] =\
-                            sortedDictArr[j+1], sortedDictArr[j]
-            i += 1
-        if inv:
-            sortedDictArr.reverse()
+        sortedDictArr = Listas.sortArrCaseIns(values, inv, noCase, sortedDictArr)
         return sortedDictArr
 
     def dictsHaveKey(dictArr, key):
-        # checks if all items in array containing dictionaries contain
-        # the informed key
+        # checks if all items in a dictionary array contain the
+        # informed key
         for d in dictArr:
             if key not in d.keys():
                 return False
@@ -176,7 +148,7 @@ class Listas:
     def unirListas(listaListas):
         if not Listas.checarListaListas(listaListas):
             return False
-        elif len(listaListas) ==  1:
+        elif len(listaListas) == 1:
             return listaListas[0]
         listaUnica = listaListas[0][:]
         for i in range(1, len(listaListas)):
