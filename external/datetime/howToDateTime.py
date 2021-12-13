d8fa7d5c3f97dec7bbc3af39fd4582a72de4a74c
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-12-13 12:19:01 -0300

    Adicionada função "todaysDate(month, day, year)", que recebe strings com os números relativos a data no formato "MM", "DD" e "YYYY": o número do mês é convertido em nome; ao número do dia, é adicionado o sufixo de numeral ordinal em inglês apropriado, em superscript ("th" para 1, "nd" para 2, "rd" para 3 e "th" para os demais casos). Por fim, a data é impressa em uma frase, e, se o mês for "December", será impressa uma mensagem de "feliz natal e próspero ano novo"; caso contrário, será desejado um "bom dia". Dessa forma, é possível imprimir mais facilmente datas informadas manualmente, evitando ter que criar variáveis com informações semelhantes às obtidas através do módulo "datetime", o que permite testar melhor o bom funcionamento do script.

--- external/datetime/howToDateTime.py
+++ external/datetime/howToDateTime.py
@@ -2,29 +2,38 @@ import datetime, sys
 sys.path.append("/home/morbi/filtering")
 from texto import Texto as T # https://github.com/tomasfn87/filtering/blob/main/texto.py
 
-months = {
-    "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6,
-    "july": 7, "august": 8, "september": 9, "october": 10, "november": 11,
-    "december": 12
-}
+def todaysDate(month, day, year):
+
+    months = {
+        "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, 
+        "june": 6, "july": 7, "august": 8, "september": 9, "october": 10, 
+        "november": 11, "december": 12
+    }
+
+    month_names_dict = months.keys()
+
+    for month_name in month_names_dict:
+        if month == str(months[month_name]):
+            month = month_name
+
+    day = T.turnIntoEnglishOrdinalNumber(day)
+    month = month.capitalize()
+
+    today = f"Today's {month} {day} {year}."
+    if month == "December":
+        today += " Merry Christmas and a Happy New Year!"
+    else:
+        today += " Have a good day!"
+
+    return today
 
 today = datetime.date.today()
-
 day = today.strftime("%d") 
 month = today.strftime("%m")
 year = today.strftime("%Y")
 
-month_names_dict = months.keys()
-
-for month_name in month_names_dict:
-    if month == str(months[month_name]):
-        month = month_name
-
-day = T.turnIntoEnglishOrdinalNumber(day)
-month = month.capitalize()
-
-print(f"Today's {month} {day} {year}.", end = " ")
-if month == "December":
-    print("Merry Christmas and a Happy New Year!")
-else:
-    print("Have a good day!")
+print(todaysDate(month, day, year))
+print(todaysDate("8", "22", "1987"))
+print(todaysDate("11", "1", "2001"))
+print(todaysDate("10", "2", "2006"))
+print(todaysDate("6", "3", "2011"))
