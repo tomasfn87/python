215e2ab9ab3895db54459560cb80bba8097d7884
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-12-13 12:52:23 -0300

    Adicionadas 2 funções: 1): "toStr(number)" e 2): "getTodaysDate()". 1) recebe uma informação numérica: se for "float", será convertido para "int", e então em "str"; se for "int" será convertido para "str", de modo a permitir que "todaysDate()" receba datas em formato numérico (e não somente em formato string). 2) usa o módulo "datetime" para retornar uma pilha contendo, respectivamente, o mês, o dia e o ano, em formato string. 3): a função "todaysDate()" não recebe mais 3 parâmetros (month, day, year) mas apenas uma pilha contendo "month", "day" e "year".

--- external/datetime/howToDateTime.py
+++ external/datetime/howToDateTime.py
@@ -2,11 +2,28 @@ import datetime, sys
 sys.path.append("/home/morbi/filtering")
 from texto import Texto as T # https://github.com/tomasfn87/filtering/blob/main/texto.py
 
-def todaysDate(month, day, year):
+def getTodaysDate():
+    today = datetime.date.today()
+    month = today.strftime("%m")
+    day = today.strftime("%d")
+    year = today.strftime("%Y")
+    return month, day, year
+
+def toStr(number):
+    assert type(number) in [str, int, float]
+    if type(number) == float:
+        number = int(number)
+    if type(number) == int:
+        number = str(number)
+    return number
+
+def todaysDate(todays_date):
+    month, day, year = todays_date[0], todays_date[1], todays_date[2]
+    month, day, year = toStr(month), toStr(day), toStr(year)
 
     months = {
-        "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, 
-        "june": 6, "july": 7, "august": 8, "september": 9, "october": 10, 
+        "january": 1, "february": 2, "march": 3, "april": 4, "may": 5,
+        "june": 6, "july": 7, "august": 8, "september": 9, "october": 10,
         "november": 11, "december": 12
     }
 
@@ -27,13 +44,11 @@ def todaysDate(month, day, year):
 
     return today
 
-today = datetime.date.today()
-day = today.strftime("%d") 
-month = today.strftime("%m")
-year = today.strftime("%Y")
+today = getTodaysDate()
 
-print(todaysDate(month, day, year))
-print(todaysDate("8", "22", "1987"))
-print(todaysDate("11", "1", "2001"))
-print(todaysDate("10", "2", "2006"))
-print(todaysDate("6", "3", "2011"))
+print(todaysDate(today))
+print(todaysDate(("8", "22", "1987")))
+print(todaysDate(("11", "1", "2001")))
+print(todaysDate(("10", "2", "2006")))
+print(todaysDate(("6", "3", "2011")))
+print(todaysDate((9, 5, 1979)))
