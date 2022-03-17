5387d1cc530425326bade919ecf52007091d41d8
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-03-17 01:16:28 -0300

    Alterado o arquivo 'external/selenium/previsao_do_tempo_brasil.py': alterada a URL de acesso ao site AccuWeather para sempre acessar a função 'current-weather', de modo a padronizar o resultado obtido. Essa mudança aumentou os dados exibidos pela função 'condicao_tempo_accuweather': não é mais exibido o 'minute cast' mas são exibidos outros dados que não eram exibidos anteriormente: 'Sensação', 'Humidade' e 'Cobertura de nuvens'.

--- external/selenium/previsao_do_tempo_brasil.py
+++ external/selenium/previsao_do_tempo_brasil.py
@@ -56,16 +56,20 @@ def condicao_tempo_accuweather(cidade, estado):
     browser.get("https://www.duckduckgo.com")
     browser.find_element(By.XPATH, '//*[@id="search_form_input_homepage"]')\
         .send_keys(f"accuweather pt br brazil weather {cidade} {estado}", Keys.ENTER)
-    browser.find_element(By.XPATH, '//*[@id="r1-0"]/div/h2/a[1]').click()
 
-    t.sleep(4)
-    browser.implicitly_wait(4)
-    
-    browser.find_element(By.CSS_SELECTOR, 'input[class="search-input"]').send_keys(f"{cidade} {estado}", Keys.ENTER)
-    
-    t.sleep(4)
-    browser.implicitly_wait(4)
-    
+    url = browser.find_element(By.XPATH, '//*[@id="r1-0"]/div/h2/a[1]').get_attribute("href")
+    url = url.replace('/en/', '/pt/')
+
+    current_weather_replacemets = ['/weather-forecast/', '/daily-weather-forecast/', '/winter-weather-forecast/', '/satellite/']
+    correct_path = '/current-weather/'
+    for wrong_path in current_weather_replacemets:
+        url = url.replace(wrong_path, correct_path)
+
+    browser.get(url)
+
+    t.sleep(5)
+    browser.implicitly_wait(3)
+
     exclude = ["de", "da", "do", "dos", "das"]
     title = f"Condições meteorológicas em {capitalize_all(cidade, exclude)}/{estado.upper()}, Brasil"
     print(title)
@@ -73,42 +77,33 @@ def condicao_tempo_accuweather(cidade, estado):
 
     # not all localities have the same kind of data available, so it needs to be wrapped in a try block
     
-    
     try:
-        temperatura_atual = browser.find_element(By.XPATH, '/html/body/div/div[5]/div[1]/div[1]/a[1]/div[1]/div[1]/div/div/div[1]')
-        temperatura_atual = temperatura_atual.text
-
-        sensacao_termica = browser.find_element(By.XPATH, '/html/body/div/div[5]/div[1]/div[1]/a[1]/div[1]/div[1]/div/div/div[2]')
-        sensacao_termica = f"{sensacao_termica.text.split(' ')[1]}C"
-
-        condicao = browser.find_element(By.XPATH, '/html/body/div/div[5]/div[1]/div[1]/a[1]/div[2]/span[1]')
-        condicao = condicao.text
+        data = browser.find_element(By.CSS_SELECTOR, 'div[class="current-weather-card card-module content-module non-ad"]').text.split("\n")
+        # print(data)
+        condicao = data[3]
+        temperatura_atual = data[2]
+        sensacao_termica = f"{data[4].split(' ')[1]}C"
+        sensacao = data[5]
+        humidade = data[11]
+        cobertura_nuvens = data[17]
     except:
         print("Informações indisponíveis. Tente novamente.")
-        # browser.quit()
+        browser.quit()
         return
-
-    ultima_atualizacao = ""
-
-    # minute cast is not always available, so it needs to be wrapped in a try block
-    try:
-        minute_cast = browser.find_element(By.XPATH, '/html/body/div/div[5]/div[1]/div[1]/a[2]/div/p')
-        ultima_atualizacao = minute_cast.text
-    except:
-        ultima_atualizacao = "indisponível"
-
-    browser.quit()
-
+    finally:
+        browser.quit()
+    
+    print(f"               Tempo: {condicao}")
     print(f"         Temperatura: {temperatura_atual}")
     print(f"    Sensação Térmica: {sensacao_termica}")
-    print(f"               Tempo: {condicao}")
-    if ultima_atualizacao != "indisponível":
-        print(f"  Última atualização: {ultima_atualizacao}")
+    print(f"            Sensação: {sensacao}")
+    print(f"            Humidade: {humidade}")
+    print(f" Cobertura de nuvens: {cobertura_nuvens}")
     
 def previsao_tempo_climatempo(cidade, estado):
     
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -117,15 +112,15 @@ def previsao_tempo_climatempo(cidade, estado):
     options.add_experimental_option("prefs", prefs)
     
     browser = webdriver.Chrome(options=options)
-    #browser = webdriver.Chrome()
+    # browser = webdriver.Chrome()
 
     browser.get("https://www.duckduckgo.com")
     browser.find_element(By.XPATH, '//*[@id="search_form_input_homepage"]')\
         .send_keys(f"climatempo {cidade} {estado} brasil", Keys.ENTER)
     browser.find_element(By.XPATH, '//*[@id="r1-0"]/div/h2/a[1]').click()
 
-    t.sleep(7)
-    browser.implicitly_wait(5)
+    t.sleep(6)
+    browser.implicitly_wait(4)
 
     data, option = [], 0
     exclude = ["de", "do", "da", "dos", "das"]
@@ -150,7 +145,7 @@ def previsao_tempo_climatempo(cidade, estado):
         previsao = data[1]
         temp_min = data[7]
         temp_max = data[8]
-        probabilidade_chuva = data[4]
+        pluviosidade = data[10]
         umidade_min = data[14]
         umidade_max = data[15]
         nascer_por_sol = ""
@@ -163,6 +158,7 @@ def previsao_tempo_climatempo(cidade, estado):
         print(f"        Temp. máxima: {temp_max}C")
         print(f"          Comparação: {comparacao}.")
         print(f"            Previsão: {previsao}")
+        print(f"        Pluviosidade: {pluviosidade}")
         print(f"      Umidade mínima: {umidade_min}")
         print(f"      Umidade máxima: {umidade_max}")
         if len(nascer_por_sol) > 0:
@@ -217,10 +213,6 @@ if __name__ == "__main__":
     elif len(inputs) > 3:
         print("Digite apenas cidade e estado; coloque aspas simples ou duplas se o nome da cidade possuir mais de uma palavra")
     else:
-        
         cidade = inputs[1].strip()
         estado = inputs[2].strip()
-        execute = True
-    
-    if execute:
         condicao_previsao_do_tempo(cidade, estado)
