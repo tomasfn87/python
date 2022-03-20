7ee238e05e3066b2f09a6e88018c7d86df7d8f18
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-03-20 01:29:21 -0300

    Adicionadas URLs de busca específicas para 1): 'package name'; 2): 'organization' e 'package name'.

--- external/selenium/check_npm_pkg_total_downloads.py
+++ external/selenium/check_npm_pkg_total_downloads.py
@@ -1,56 +1,51 @@
 import time as t
 import datetime as dt
-from selenium import webdriver # control the browser
-from selenium.webdriver.common.keys import Keys # keyboard
-from selenium.webdriver.common.by import By # access HTML elements
+from selenium import webdriver  # control the browse
+from selenium.webdriver.common.by import By  # access HTML elements
 
 class NpmPackage:
     def __init__(self, package_name, publication_dateDMY):
-        # '@organization/name' or 'name'
         self.pname = package_name
-        
-        # DD-MM-YYYY
         self.pdateDMY = publication_dateDMY
-    
+
     def show_total_downloads(self, show=True):
-        # YYYY-MM-DD -> DD-MM-YYYY
         todayYMD = str(dt.date.today())
         todayDMY = f"{todayYMD[8:10]}-{todayYMD[5:7]}-{todayYMD[0:4]}"
-        
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-
-        browser = webdriver.Chrome(options=options)
 
         package_info = self.pname.split("/")
-        organization = package_info[0]
-        package_name = package_info[1]
-        
-        # DD-MM-YYYY -> YYYY-MM-DD
-        pdateDMY = self.pdateDMY.split("-")
-        pdateYMD = f"{pdateDMY[2]}-{pdateDMY[1]}-{pdateDMY[0]}"
+        organization, package_name = "", ""
 
-        # format 'npm-stat' package search from publication date to today
-        npm_stat_url = f"https://npm-stat.com/charts.html?package={organization}%2F{package_name}&from={pdateYMD}&to={todayYMD}"
+        if len(package_info) == 1:
+            package_name = package_info[0]
+        elif len(package_info) == 2:
+            organization, package_name = package_info[0], package_info[1]
 
+        pdateYMD = self.pdateDMY.split("-")
+        pdateYMD = f"{pdateYMD[2]}-{pdateYMD[1]}-{pdateYMD[0]}"
+
+        npm_stat_url = "https://npm-stat.com/charts.html?package="
+        if organization:
+            npm_stat_url += f"{organization}%2F"
+        npm_stat_url += f"{package_name}&from={pdateYMD}&to={todayYMD}"
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        browser = webdriver.Chrome(options=options)
         browser.get(npm_stat_url)
-
         t.sleep(1)
         browser.implicitly_wait(1)
-
-        # get total downloads from <td>
-        td_list = browser.find_elements(By.TAG_NAME, 'td')
-        total_downloads = td_list[-1].text
-
+        total_downloads = browser.find_elements(By.TAG_NAME, 'td')[-1].text
         browser.quit()
 
         if show == True:
             print(f"package {self.pname}'s")
             print(f"Total downloads from {self.pdateDMY.replace('-', '/')}")
             print(f"                  to {todayDMY.replace('-', '/')}: {total_downloads}")
-        else:
-            return(int(total_downloads.replace(",", "")))
 
-pkg = NpmPackage(package_name="@nighly/sort-object-array-by-property", publication_dateDMY="02-02-2022")
+        return int(total_downloads.replace(",", ""))
 
-pkg.show_total_downloads()
+pkg = NpmPackage(
+    package_name="@nighly/sort-object-array-by-property",
+    publication_dateDMY="02-02-2022")
+
+print(pkg.show_total_downloads())
