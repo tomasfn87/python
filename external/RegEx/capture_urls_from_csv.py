95e723e1fc2fa8b169369bb930754bc938dcd26f
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-25 18:58:29 -0300

    Alterado o arquivo '/external/RegEx/capture_urls_from_csv.py': alterada a expressão regular, para que não permita os caracteres '}', ']' ou ')' como último elemento de uma URL, porém permitindo seu uso no corpo de uma URL.

--- external/RegEx/capture_urls_from_csv.py
+++ external/RegEx/capture_urls_from_csv.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
             data = fh.read()
 
         csvLines = data.split('\n')
-        rePossibleWebsiteUrl = r"(?i)((ht{1,2}ps?:(\/{1,2}|\\{1,2}))?(w{3}\.)?([a-z][a-z0-9-]+\.)+([a-z][a-z0-9-]+)((\/|\\)[^\'\"\s\[\]\(\)\{\}]*)*\/?)"
+        rePossibleWebsiteUrl = r"(?i)((ht{1,2}ps?:(\/{1,2}|\\{1,2}))?(w{3}\.)?([a-z][a-z0-9-]*\.)+([a-z][a-z0-9-]*)((\/|\\)[^\\\/\s]+)*\S?\b[\\\/]?)"
         urls = []
-        
+
         for i in range(0, len(csvLines)-1):
             if re.findall(rePossibleWebsiteUrl, csvLines[i]):
                 for j in re.findall(rePossibleWebsiteUrl, csvLines[i]):
