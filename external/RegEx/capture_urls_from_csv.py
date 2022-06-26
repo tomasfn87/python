0fb662819425092e6b94ff1c98d1bb63e7f15937
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-06-25 22:10:00 -0300

    Alterado o arquivo '/external/RegEx/capture_urls_from_csv.py': 1): adicionado indicador do número total de linhas analisadas (excluindo as linhas vazias). 2): foi criada a função 'getURLsFromCsvFile', que separa a análise do arquivo da verificação e processamento dos argumentos via CLI.

--- external/RegEx/capture_urls_from_csv.py
+++ external/RegEx/capture_urls_from_csv.py
@@ -1,29 +1,32 @@
 import re
 import sys
 
+def getURLsFromCsvFile(csvFile):
+    with open(csvFile, 'r') as fh:
+        data = fh.read()
+
+    csvLines = data.split('\n')
+    for i in range(0, len(csvLines)):
+        if csvLines[i] == "":
+            csvLines.pop(i)
+
+    rePossibleWebsiteUrl = r"(?i)(([fh]t{1,2}ps?:(\/{1,2}|\\{1,2}))?(w{3}\.)?([a-z][a-z0-9-]*)(\.[a-z][a-z0-9-]*)+((\/|\\)[^\\\/\s]+)*\S?\b[\\\/]?)"
+    urls = []
+    for i in range(0, len(csvLines)-1):
+        if re.findall(rePossibleWebsiteUrl, csvLines[i]):
+            for j in re.findall(rePossibleWebsiteUrl, csvLines[i]):
+                urls.append({
+                    'url': j[0],
+                    'line': i+1
+                })
+
+    numDigitsMatchNumber = len(str(len(urls)))
+    print(f'Matches ({len(urls)}/{len(csvLines)}):')
+    for i in range(0, len(urls)):
+        print(f'* {str(i+1).rjust(numDigitsMatchNumber, " ")} - {urls[i]["url"]} ({urls[i]["line"]})')
+
 if __name__ == '__main__':
     inputs = sys.argv
-
     if len(inputs) > 1:
         inputFile = sys.argv[1]
-
-        with open(inputFile, 'r') as fh:
-            data = fh.read()
-
-        csvLines = data.split('\n')
-        rePossibleWebsiteUrl = r"(?i)(([fh]t{1,2}ps?:(\/{1,2}|\\{1,2}))?(w{3}\.)?([a-z][a-z0-9-]*)(\.[a-z][a-z0-9-]*)+((\/|\\)[^\\\/\s]+)*\S?\b[\\\/]?)"
-        urls = []
-
-        for i in range(0, len(csvLines)-1):
-            if re.findall(rePossibleWebsiteUrl, csvLines[i]):
-                for j in re.findall(rePossibleWebsiteUrl, csvLines[i]):
-                    urls.append({
-                        'url': j[0],
-                        'line': i+1
-                    })
-                    
-        numDigitsMatchNumber = len(str(len(urls)))
-
-        print(f'Matches ({len(urls)}):')
-        for i in range(0, len(urls)):
-            print(f'* {str(i+1).rjust(numDigitsMatchNumber, " ")} - {urls[i]["url"]} ({urls[i]["line"]})')
+        getURLsFromCsvFile(inputFile)
