9b51c2d52a36f4d5e1087d62d7c998e0233c432a
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-11-29 23:07:49 -0300

    Alterado arquivo 'external/selenium/previsao_do_tempo_brasil.py': adicionadas classes 'Results' e 'ResultsPrinter': têm como função coletar e processar os resultados obtidos antes de imprimí-los, permitindo dessa forma que o comprimento da margem à esquerda da exibição no console seja automaticamente definido.

--- external/selenium/previsao_do_tempo_brasil.py
+++ external/selenium/previsao_do_tempo_brasil.py
@@ -8,35 +8,39 @@ from selenium.webdriver.common.keys import Keys
 from typing import Any, Dict, List, Union
 
 def main():
-    inputs = sys.argv
+    inputs:list[str] = sys.argv
     if len(inputs) < 3:
         print("ERRO: é necessário digitar cidade e estado.")
         print_examples()
         return
     elif len(inputs) > 4:
-        print("ERRO: digite apenas cidade e estado; coloque aspas simples ou duplas")
-        print("  se o nome da cidade possuir mais de uma palavra ou utilize")
-        print(r"  a barra invertida (\) para cancelar um espaço em branco como")
-        print("  separador de argumentos.")
+        print("ERRO: digite apenas cidade e estado; coloque aspas simples ou")
+        print("  duplas  se o nome da cidade possuir mais de uma palavra ou")
+        print("    utilize  a barra invertida (\) para cancelar um espaço ")
+        print("      vazio como separador de argumentos.")
         print_examples()
         return
 
     cidade, estado = inputs[1], inputs[2]
-    padding = 33
-    if not isStringAValidFixedLengthAcronym(s=estado, length=2, acronym_list=estados_brasileiros):
-        print("ERRO: o segundo argumento deve ser uma sigla válida de estado brasileiro (UF).")
-        print("\n - Siglas válidas: {}.".format(
-            conectar(getBrazilianStatesAcronymsList(lista_estados=estados_brasileiros), ' ou ')))
+    if not isStringAValidFixedLengthAcronym(
+        s=estado, length=2, acronym_list=estados_brasileiros):
+        print("ERRO: o segundo argumento deve ser uma sigla válida ", end='')
+        print("de estado brasileiro (UF).")
+        print("\n  Siglas válidas:\n  ---------------\n\n    {}.".format(
+            join(list_brazilian_states_acronyms_list(
+                lista_estados=estados_brasileiros), 'ou')))
     elif len(inputs) == 4:
         headless = inputs[3]
         
         if re.match('(?i)^(true|false)$', headless):
             if re.match('(?i)true', headless):
-                condicao_previsao_do_tempo(cidade=cidade, estado=estado, padding=padding, headless=True)
+                condicao_previsao_do_tempo(
+                    cidade=cidade, estado=estado, headless=True)
             if re.match('(?i)false', headless):
-                condicao_previsao_do_tempo(cidade=cidade, estado=estado, padding=padding, headless=False)
+                condicao_previsao_do_tempo(
+                    cidade=cidade, estado=estado, headless=False)
     else:
-        condicao_previsao_do_tempo(cidade=cidade, estado=estado, padding=padding)
+        condicao_previsao_do_tempo(cidade=cidade, estado=estado)
 
 def print_examples():
     print("\n - Exemplo 1:")
@@ -47,34 +51,14 @@ def print_examples():
     print("\t", end="")
     print(r"python3 previsao_do_tempo_brasil.py rio\ de\ janeiro rj")
 
-def isStringAValidFixedLengthAcronym(s:str, length:int, acronym_list:List[Dict[str, str]]):
+def isStringAValidFixedLengthAcronym(
+    s:str, length:int, acronym_list:List[Dict[str, str]]):
+    
     if len(s.strip()) != length:
         return False
     return any(item['acronym'] == s.strip().upper() for item in acronym_list)
 
-def conectar(lista_de_itens:List[Union[Any, str]], espacador_1:str=" e ", espacador_2:str=", "):
-        texto = ""
-        for i in range(0, len(lista_de_itens)):
-            texto += str(lista_de_itens[i])
-            if i == len(lista_de_itens) - 1:
-                return texto
-            elif i == len(lista_de_itens) - 2:
-                texto += espacador_1
-            else:
-                texto += espacador_2
-
-def getBrazilianStatesAcronymsList(lista_estados:List[Dict[str, str]]):
-    return [f"{estado['acronym']} ({estado['name']})" for estado in lista_estados]
-
-def condicao_previsao_do_tempo(cidade:str, estado:str, padding:int, headless:bool=False):
-    data_hora = f"Data/hora: {str(dt.datetime.now())[0:19]}"
-    print(f"{data_hora}\n{'-' * len(data_hora)}")
-    print()
-    condicao_tempo_accuweather(cidade=cidade, estado=estado, padding=padding, headless=headless)
-    print()
-    previsao_tempo_climatempo(cidade=cidade, estado=estado, padding=padding, headless=headless)
-
-estados_brasileiros = [
+estados_brasileiros:List[Dict[str, str]] = [
     {"acronym": "AC", "name": "Acre"},
     {"acronym": "AL", "name": "Alagoas"},
     {"acronym": "AP", "name": "Amapá"},
@@ -103,67 +87,192 @@ estados_brasileiros = [
     {"acronym": "SE", "name": "Sergipe"},
     {"acronym": "TO", "name": "Tocantins"}]
 
-def condicao_tempo_accuweather(cidade:str, estado:str, padding:int, headless:bool=False):
+def join(lista_de_itens:List[Union[Any, str]], 
+        espacador_1:str="e", espacador_2:str=",") -> str:
+    
+        texto = ""
+        for i in range(0, len(lista_de_itens)):
+            texto += str(lista_de_itens[i])
+            if i == len(lista_de_itens) - 1:
+                return texto
+            elif i == len(lista_de_itens) - 2:
+                texto += f" {espacador_1} "
+            else:
+                texto += f"{espacador_2} "
+
+def list_brazilian_states_acronyms_list(lista_estados:List[Dict[str, str]]):
+    return [f"{estado['acronym']} ({estado['name']})"
+        for estado in lista_estados]
+
+def condicao_previsao_do_tempo(cidade:str, estado:str, headless:bool=False):
+    resultados_condicao_tempo = condicao_tempo_accuweather(
+        cidade=cidade, estado=estado, headless=headless)
+    resultados_previsao_tempo = previsao_tempo_climatempo(
+        cidade=cidade, estado=estado, headless=headless)
+    results_printer = ResultsPrinter()
+    results_printer.add(resultados_condicao_tempo)
+    results_printer.add(resultados_previsao_tempo)
+    results_printer.print_all()
+
+class Results:
+    def __init__(self:Any, title:str):
+        self.title:str = title
+        self.results:List[Dict[str, str]] = []
+    
+    def add(self:Any, result:Dict[str, str]):
+        if list(result.keys())[0].strip() \
+            and list(result.values())[0].strip():
+            self.results.append(result)
+        else:
+            print("Resultado não foi adicionado pois ou a chave ou")
+        
+    def get_title(self:Any) -> str:
+        return self.title
+
+    def get_max_key_length(self:Any) -> int:
+        max_title_length:int = 0
+        for i in range(len(self.results)):
+            if len(list(self.results[i].keys())[0]) > max_title_length:
+                max_title_length = len(list(self.results[i].keys())[0])
+        return max_title_length
+        
+class ResultsPrinter:
+    def __init__(self:Any):
+        self.result_list:List[Results] = []
+    
+    def add(self:Any, results:Results):
+        self.result_list.append(results)
+        
+    def print_all(self:Any):
+        data_hora = f"Data/hora: {str(dt.datetime.now())[0:19]}"
+        print(f"{data_hora}\n{'-' * len(data_hora)}\n")
+        padding, r_list = 0, self.result_list
+        
+        for r in r_list:
+            if r.get_max_key_length() > padding:
+                padding = r.get_max_key_length()
+        padding += 2
+        
+        for i in range(len(r_list)):
+            title = r_list[i].get_title()
+            print(f"{title}\n{'-' * len(title)}")
+            for j in range(len(r_list[i].results)):
+                print(
+                    f"{list(r_list[i].results[j].keys())[0].rjust(padding)}:",
+                        end='')
+                print(f" {list(r_list[i].results[j].values())[0]}")
+                if j is len(r_list[i].results) - 1 \
+                    and i is not len(self.result_list) - 1:
+                    print()
+
+def condicao_tempo_accuweather(
+    cidade:str, estado:str, headless:bool=False) -> Results:
+    
     browser = start_chrome(headless)
     browser.get("https://www.duckduckgo.com")
-    browser.find_element(By.CSS_SELECTOR, 'input[type=text]')\
-        .send_keys(f"accuweather pt br brazil weather {cidade} {estado}", Keys.ENTER)
-    url = browser.find_element(By.CSS_SELECTOR, '#r1-0 h2 a').get_attribute("href")
+    browser.find_element(
+        By.CSS_SELECTOR, 'input[type=text]').send_keys(
+            f"accuweather pt br brazil weather {cidade} {estado}", Keys.ENTER)
+    url = browser.find_element(
+        By.CSS_SELECTOR, '#r1-0 h2 a').get_attribute("href")
     browser.get(format_accuweather_url(url))
     t.sleep(2)
     browser.implicitly_wait(2)
-    title = f"[AccuWeather] Condições meteorológicas em {capitalize_all(cidade)}/{estado.upper()}, Brasil"
-    print(f"{title}\n{'-' * len(title)}")
-    TempoAtual = browser.find_element(By.CSS_SELECTOR, '.current-weather-card h1').text
-    tempoAtual = browser.find_element(By.CSS_SELECTOR, '.current-weather-card div.current-weather-info div.temp').text
-    tempoAtualSensacao = browser.find_element(By.CSS_SELECTOR, '.current-weather-card div.phrase').text
-    tempoAtualExtraRealFeelData = browser.find_element(By.CSS_SELECTOR, '.current-weather-card div.current-weather-extra').text.split('\n')
-    tempoAtualExtraRealFeelTitulo = ''
-    tempoAtualExtraRealFeelValor = ''
+    
+    title = "[AccuWeather] Condições meteorológicas em "
+    title += f"{capitalize_all(cidade)}/{estado.upper()}, Brasil"
+    results = Results(title)
+
+    TempoAtual = browser.find_element(
+        By.CSS_SELECTOR, '.current-weather-card h1').text
+    
+    tempoAtual = browser.find_element(
+        By.CSS_SELECTOR,
+            '.current-weather-card div.current-weather-info div.temp').text
+    
+    tempoAtualSensacao = browser.find_element(
+        By.CSS_SELECTOR, '.current-weather-card div.phrase').text
+    
+    tempoAtualExtraRealFeelData = browser.find_element(
+        By.CSS_SELECTOR, '.current-weather-card div.current-weather-extra'
+            ).text.split('\n')
+    
+    weather_details = browser.find_element(
+        By.CSS_SELECTOR, '.current-weather-card .current-weather-details'
+            ).text.split('\n')
+    
+    browser.quit()
+    
     tempoAtualExtraRealFeelTitulo = tempoAtualExtraRealFeelData[0].split(' ')[0]
     tempoAtualExtraRealFeelValor = tempoAtualExtraRealFeelData[0].split(' ')[1]
+    
+    tempoAtualExtraRealFeelShadeTitulo = ''
+    tempoAtualExtraRealFeelShadeValor = ''
+    
     if len(tempoAtualExtraRealFeelData) == 2:
-        tempoAtualExtraRealFeelShadeTitulo = tempoAtualExtraRealFeelData[1].split(' ')[0]+tempoAtualExtraRealFeelData[1].split(' ')[1]
-        tempoAtualExtraRealFeelShadeValor = tempoAtualExtraRealFeelData[1].split(' ')[2]
-    print(f"{TempoAtual.rjust(padding)}: {tempoAtual} ({tempoAtualSensacao})")
-    print(f"{tempoAtualExtraRealFeelTitulo.rjust(padding)}: {tempoAtualExtraRealFeelValor}C")
-    if len(tempoAtualExtraRealFeelData) == 2:
-        print(f"{tempoAtualExtraRealFeelShadeTitulo.rjust(padding)}: {tempoAtualExtraRealFeelShadeValor}C")
-    weather_details = browser.find_element(By.CSS_SELECTOR, '.current-weather-card .current-weather-details').text.split('\n')
+        tempoAtualExtraRealFeelShadeTitulo = \
+            tempoAtualExtraRealFeelData[1].split(' ')[0] \
+                + tempoAtualExtraRealFeelData[1].split(' ')[1]
+
+        tempoAtualExtraRealFeelShadeValor = \
+            tempoAtualExtraRealFeelData[1].split(' ')[2]
+    
+    results.add({TempoAtual:
+        f"{tempoAtual} ({tempoAtualSensacao})"})
+
+    results.add({tempoAtualExtraRealFeelTitulo:
+        f"{tempoAtualExtraRealFeelValor}C"})
+    
+    if tempoAtualExtraRealFeelShadeTitulo and tempoAtualExtraRealFeelShadeValor:
+        results.add({tempoAtualExtraRealFeelShadeTitulo:
+            f"{tempoAtualExtraRealFeelShadeValor}C"})
+ 
     for i in range(len(weather_details)):
         if i % 2 == 0:
-            print(f'{weather_details[i].rjust(padding)}: {weather_details[i+1].replace("° C", "°C")}')
-    browser.quit()
+            results.add({f"{weather_details[i]}":
+                f"{weather_details[i+1].replace('° C', '°C')}"})
+            
+    return results
 
-def previsao_tempo_climatempo(cidade:str, estado:str, padding:int, headless:bool=False):
+def previsao_tempo_climatempo(cidade:str, estado:str, headless:bool=False) -> Results:
     browser = start_chrome(headless)
     browser.get("https://www.duckduckgo.com")
-    browser.find_element(By.CSS_SELECTOR, "input[type=text]")\
-        .send_keys(f"climatempo {cidade} {estado} brasil", Keys.ENTER)
-    browser.find_element(By.CSS_SELECTOR, "#r1-0 h2 a").click()
+    
+    browser.find_element(
+        By.CSS_SELECTOR, "input[type=text]").send_keys(
+            f"climatempo {cidade} {estado} brasil", Keys.ENTER)
 
+    browser.find_element(
+        By.CSS_SELECTOR, "#r1-0 h2 a").click()
+    
     t.sleep(2)
     browser.implicitly_wait(2)
-
+    
     data, option = [], 0
-
-    title = f"[ClimaTempo] Previsão do tempo em {capitalize_all(cidade)}/{estado.upper()}, Brasil"
-    print(f"{title}\n{'-' * len(title)}")
+    title = "[ClimaTempo] Previsão do tempo em "
+    title += f"{capitalize_all(cidade)}/{estado.upper()}, Brasil"
+    results = Results(title)
 
     try:
-        data = browser.find_element(By.CSS_SELECTOR, 'div[class="card -no-top -no-bottom"]').text.split("\n")
+        data = browser.find_element(
+            By.CSS_SELECTOR,
+            'div[class="card -no-top -no-bottom"]').text.split("\n")
+
         option = 1
     except:
         try:
-            data = browser.find_element(By.CSS_SELECTOR, "#first-block-of-days section")
+            data = browser.find_element(
+                By.CSS_SELECTOR, "#first-block-of-days section")
+
             option = 2
             data = remove_empty_elements(data.text.split("\n"))
         except:
             print("Informações indisponíveis. Tente novamente.")
             browser.quit()
             return
+    
+    browser.quit()
 
-    padding = padding + 2
     if option == 1:
         comparacao = data[0]
         previsao = limit_empty_spaces(data[1])
@@ -178,16 +287,16 @@ def previsao_tempo_climatempo(cidade:str, estado:str, padding:int, headless:bool
             if data[i] == 'Sol':
                 nascer_por_sol = data[i+1].replace('h', '')
 
-        
-        print("Temp. mínima: ".rjust(padding)+f"{temp_min}C")
-        print("Temp. máxima: ".rjust(padding)+f"{temp_max}C")
-        print("Comparação: ".rjust(padding)+comparacao)
-        print("Previsão: ".rjust(padding)+limit_empty_spaces(previsao))
-        print("Precipitação: ".rjust(padding)+precipitacao)
-        print("Humidade mínima: ".rjust(padding)+umidade_min)
-        print("Humidade máxima: ".rjust(padding)+umidade_max)
+        results.add({"Temperatura mínima": f"{temp_min}C"})    
+        results.add({"Temperatura máxima": f"{temp_max}C"})    
+        results.add({"Comparação": comparacao})    
+        results.add({"Previsão": limit_empty_spaces(previsao)})
+        results.add({"Precipitação": precipitacao})
+        results.add({"Humidade mínima": umidade_min})
+        results.add({"Humidade máxima": umidade_max})
         if nascer_por_sol:
-            print("Nascer/pôr do sol: ".rjust(padding)+nascer_por_sol.replace(' ', ' / '))
+            results.add({
+                "Nascer/pôr do sol": nascer_por_sol.replace(' ', ' / ')})
 
     elif option == 2:
         temp_min = data[2]
@@ -210,33 +319,35 @@ def previsao_tempo_climatempo(cidade:str, estado:str, padding:int, headless:bool
             if data[i] == 'LUA':
                 lua = data[i+1]
 
-        browser.quit()
+        results.add({"Temperatura mínima": f"{temp_min}C"})
+        results.add({"Temperatura máxima": f"{temp_max}C"})
+        results.add({"Previsão": previsao})
+        results.add({"Pluviosidade": pluviosidade})
+        results.add({"Umidade": umidade})
+        results.add({"Nascer/pôr do sol": nascer_por_sol.replace('-', '/')})
+        results.add({"Lua": lua})
 
-        print("Temp. mínima: ".rjust(padding)+f"{temp_min}C")
-        print("Temp. máxima: ".rjust(padding)+f"{temp_max}C")
-        print("Previsão: ".rjust(padding)+previsao)
-        print("Pluviosidade: ".rjust(padding)+pluviosidade)
-        print("Umidade: ".rjust(padding)+umidade)
-        print("Nascer/pôr do sol: ".rjust(padding)+nascer_por_sol.replace(' - ', ' / '))
-        print("Lua: ".rjust(padding)+lua)
+    return results
 
 def start_chrome(headless:bool=False):
     options = webdriver.ChromeOptions()
     headless and options.add_argument('--headless')
     options.add_argument('--disable-extensions')
     options.add_argument('--profile-directory=Default')
-    options.add_argument("--incognito")
-    options.add_argument("--disable-plugins-discovery");
+    options.add_argument('--incognito')
+    options.add_argument('--disable-plugins-discovery')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     prefs = {"profile.managed_default_content_settings.images": 2}
     options.add_experimental_option("prefs", prefs)
+    
     return webdriver.Chrome(options=options)
 
 def format_accuweather_url(url:str):
-    return url.replace('en', 'pt').replace('weather-forecast', 'current-weather')
+    return url.replace('en', 'pt').replace(
+        'weather-forecast', 'current-weather')
 
 def capitalize_all(text:str):
     exclude = ["de", "da", "do", "dos", "das"]
